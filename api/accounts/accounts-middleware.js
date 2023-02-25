24f5a76d61e582150ace2d48a36249dc5e59637f
ehrstj <126328992+juhth7@users.noreply.github.com>
2023-02-25 20:00:54 +0800

    Add files via upload

---
+++ api/accounts/accounts-middleware.js
@@ -0,0 +1,13 @@
+exports.checkAccountPayload = (req, res, next) => {
+  // DO YOUR MAGIC
+  // Note: you can either write "manual" validation logic
+  // or use the Yup library (not currently installed)
+}
+
+exports.checkAccountNameUnique = (req, res, next) => {
+  // DO YOUR MAGIC
+}
+
+exports.checkAccountId = (req, res, next) => {
+  // DO YOUR MAGIC
+}
